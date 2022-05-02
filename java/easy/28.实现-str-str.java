4535be1607dcee3180b2eabdb6791d7142e33c25
Lei YAN <7792731@qq.com>
2022-05-02 23:24:48 +0800

    Problem per day: 28. Implement strStr()

--- java/easy/28.实现-str-str.java
+++ java/easy/28.实现-str-str.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int strStr(String haystack, String needle) {
-
+        // 暴力解法
+        if (needle.length() == 0) return 0;
+        int m = haystack.length();
+        int n = needle.length();
+        for (int i = 0; i <= m - n; i++) {
+            int a = i; // 原串匹配起始位置，尝试匹配
+            int b = 0; // 匹配长度
+            while (b < n && haystack.charAt(a) == needle.charAt(b)) {
+                a++;
+                b++;
+            }
+            if (b == n) return i;            
+        }
+        return -1;
     }
 }
 // @lc code=end
