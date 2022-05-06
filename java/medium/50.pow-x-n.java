9fa084e07732e7f1a6c5ec1ed6416d69ea5396ca
Lei YAN <7792731@qq.com>
2022-05-06 22:51:46 +0800

    Problem per day: 50. Pow(x, n)

---
+++ java/medium/50.pow-x-n.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;        
+        double half = myPow(x, n / 2);
+        double rest = myPow(x, n % 2);
+        return half * half * rest;
+    }
+}
+// @lc code=end
+
