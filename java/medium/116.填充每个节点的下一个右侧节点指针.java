b64dd4d35c3d8387e9d06ab375ae2574fd39b91d
Lei YAN <7792731@qq.com>
2022-05-09 23:23:10 +0800

    Problem per day: 116. Populating Next Right Pointers in Each Node

---
+++ java/medium/116.填充每个节点的下一个右侧节点指针.java
@@ -0,0 +1,68 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.w3c.dom.Node;
+
+/*
+ * @lc app=leetcode.cn id=116 lang=java
+ *
+ * [116] 填充每个节点的下一个右侧节点指针
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        // 层序遍历
+        if(root == null) return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            // 当前层的大小
+            int size = queue.size();
+            // 遍历当前层
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+
+                // 进入下一层
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            
+        }
+        
+        return root;
+    }
+}
+// @lc code=end
+
