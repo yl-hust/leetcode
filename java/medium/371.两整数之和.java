c8601c57cd51443c444e913889469f446873767e
Lei YAN <7792731@qq.com>
2022-05-10 16:10:31 +0800

    Problem per day: 371. Sum of Two Integers

---
+++ java/medium/371.两整数之和.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=371 lang=java
+ *
+ * [371] 两整数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int getSum(int a, int b) {
+        // 无进位加法用^，进位加法用&和左移<<1
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+}
+// @lc code=end
+
