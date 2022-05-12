855363e6b5de1e76a533d9a150d03d5a3cf83088
Lei YAN <7792731@qq.com>
2022-05-12 22:18:23 +0800

    Problem per day: 103. Binary Tree Zigzag Level Order Traversal

---
+++ java/medium/103.二叉树的锯齿形层序遍历.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=103 lang=java
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        boolean leftToRight = true; // 左->右
+        while (!q.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (leftToRight) {
+                    level.add(node.val);
+                } else {
+                    level.add(0, node.val);
+                }
+                // 压栈总是先左后右
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }                
+            }
+            leftToRight = !leftToRight;
+            ans.add(level);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
