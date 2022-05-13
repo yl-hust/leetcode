261c333a4ba1b5fd02c42a151f069c94dc8eafb9
Lei YAN <7792731@qq.com>
2022-05-13 22:19:07 +0800

    Problem per day: 162. Find Peak Element

---
+++ java/medium/162.寻找峰值.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=java
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+        // 因为nums[-1]=nums[n]=负无穷,所以即使单调递增或递减，也有解
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+   }
+
+}
+// @lc code=end
+
