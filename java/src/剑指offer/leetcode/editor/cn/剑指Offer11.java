c7b5b2207b7e1194561d4d3b9c0acb3b4f5317a4
Lei YAN <7792731@qq.com>
2022-05-17 12:16:47 +0800

    Problem per day: 剑指Offer11.旋转数组的最小数字

---
+++ java/src/剑指offer/leetcode/editor/cn/剑指Offer11.java
@@ -0,0 +1,71 @@
+package 剑指offer.leetcode.editor.cn;
+//把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。 
+//
+// 给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组 [3
+//,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。 
+//
+// 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], 
+//..., a[n-2]] 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：numbers = [3,4,5,1,2]
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：numbers = [2,2,2,0,1]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == numbers.length 
+// 1 <= n <= 5000 
+// -5000 <= numbers[i] <= 5000 
+// numbers 原来是一个升序排序的数组，并进行了 1 至 n 次旋转 
+// 
+//
+// 注意：本题与主站 154 题相同：https://leetcode-cn.com/problems/find-minimum-in-rotated-sor
+//ted-array-ii/ 
+// Related Topics 数组 二分查找 
+// 👍 619 👎 0
+
+
+// 旋转数组的最小数字
+public class 剑指Offer11 {
+    public static void main(String[] args) {
+        Solution solution = new 剑指Offer11().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minArray(int[] numbers) {
+        int lo = 0;
+        int hi = numbers.length - 1;
+        // 根据题意，最后一个元素一定是>=min的
+        // 最小值左侧 最小值 最小值右侧
+        // 输入：numbers = [3,4,5,1,2]
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (numbers[mid] > numbers[hi]) { // mid位于最小值左侧
+                lo = mid + 1;
+            } else if (numbers[mid] < numbers[hi]) { // mid位于最小值右侧
+                hi = mid;
+            } else { // 无法确定，右指针左移
+                hi--;
+            }
+        }
+        return numbers[lo];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
