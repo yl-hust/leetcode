9549947602f4ca07b80be968651d8ba9eeb96295
Lei YAN <7792731@qq.com>
2022-05-20 08:39:14 +0800

    剑指 Offer 57. 和为s的两个数字

---
+++ java/src/剑指offer/leetcode/editor/cn/剑指Offer57.java
@@ -0,0 +1,55 @@
+package 剑指offer.leetcode.editor.cn;
+//输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [2,7,11,15], target = 9
+//输出：[2,7] 或者 [7,2]
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [10,26,30,31,47,60], target = 40
+//输出：[10,30] 或者 [30,10]
+// 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 <= nums.length <= 10^5 
+// 1 <= nums[i] <= 10^6 
+// 
+// Related Topics 数组 双指针 二分查找 
+// 👍 188 👎 0
+
+
+// 和为s的两个数字
+public class 剑指Offer57 {
+    public static void main(String[] args) {
+        Solution solution = new 剑指Offer57().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // Two pointers: collision pointer
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == target) {
+                return new int[]{nums[i], nums[j]};
+            } else if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return new int[0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
