f3ee093bb5aae0bbcaca628572fee6b379300863
Lei YAN <7792731@qq.com>
2022-05-26 16:16:34 +0800

    剑指 Offer 59 - II. 队列的最大值

---
+++ java/src/剑指offer/leetcode/editor/cn/剑指Offer59II.java
@@ -0,0 +1,86 @@
+package 剑指offer.leetcode.editor.cn;
+//请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都
+//是O(1)。 
+//
+// 若队列为空，pop_front 和 max_value 需要返回 -1 
+//
+// 示例 1： 
+//
+// 输入: 
+//["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
+//[[],[1],[2],[],[],[]]
+//输出: [null,null,null,2,1,2]
+// 
+//
+// 示例 2： 
+//
+// 输入: 
+//["MaxQueue","pop_front","max_value"]
+//[[],[],[]]
+//输出: [null,-1,-1]
+// 
+//
+// 
+//
+// 限制： 
+//
+// 
+// 1 <= push_back,pop_front,max_value的总操作数 <= 10000 
+// 1 <= value <= 10^5 
+// 
+// Related Topics 设计 队列 单调队列 
+// 👍 370 👎 0
+
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// 队列的最大值
+public class 剑指Offer59II {
+    public static void main(String[] args) {
+        MaxQueue solution = new 剑指Offer59II().new MaxQueue();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class MaxQueue {
+    Queue<Integer> data;
+    Deque<Integer> max; // 单调队列
+    public MaxQueue() {
+        data = new LinkedList<>();
+        max = new LinkedList<>();
+    }
+    
+    public int max_value() {
+        return max.isEmpty() ? -1 : max.peekFirst();
+    }
+    
+    public void push_back(int value) {
+        data.offer(value);
+        while (!max.isEmpty() && max.peekLast() < value) {
+            max.pollLast();
+        }
+        max.offerLast(value);
+    }
+    
+    public int pop_front() {
+        if (data.isEmpty()) {
+            return -1;
+        }
+        int ans = data.poll();
+        if (ans == max.peekFirst()) {
+            max.pollFirst();
+        }
+        return ans;
+    }
+}
+
+/**
+ * Your MaxQueue object will be instantiated and called as such:
+ * MaxQueue obj = new MaxQueue();
+ * int param_1 = obj.max_value();
+ * obj.push_back(value);
+ * int param_3 = obj.pop_front();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
