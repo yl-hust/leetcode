c14ad72a4eae3c03d91260251c95435c7e1d557a
Lei YAN <7792731@qq.com>
2022-05-26 22:17:10 +0800

    剑指 Offer 26. 树的子结构

---
+++ java/src/剑指offer/leetcode/editor/cn/剑指Offer26.java
@@ -0,0 +1,72 @@
+package 剑指offer.leetcode.editor.cn;
+//输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构) 
+//
+// B是A的子结构， 即 A中有出现和B相同的结构和节点值。 
+//
+// 例如: 
+//给定的树 A: 
+//
+// 3 
+// / \ 
+// 4 5 
+// / \ 
+// 1 2 
+//给定的树 B： 
+//
+// 4 
+// / 
+// 1 
+//返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。 
+//
+// 示例 1： 
+//
+// 输入：A = [1,2,3], B = [3,1]
+//输出：false
+// 
+//
+// 示例 2： 
+//
+// 输入：A = [3,4,5,1,2], B = [4,1]
+//输出：true 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 10000 
+// Related Topics 树 深度优先搜索 二叉树 
+// 👍 560 👎 0
+
+
+// 树的子结构
+public class 剑指Offer26 {
+    public static void main(String[] args) {
+        Solution solution = new 剑指Offer26().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) return false;
+        // if A.val == B.val, then compare children recursively
+        // else recursively A.left == B.val and A.right == B.val
+        return isSub(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    private boolean isSub(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null) return false; // B != null
+        // both A and B not null
+        if (A.val != B.val) return false;
+        return isSub(A.left, B.left) && isSub(A.right, B.right);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
