3618f8a51861dbee0376f3269cd7f30a5a32bd1f
Lei YAN <7792731@qq.com>
2022-06-05 22:12:32 +0800

    32. Longest Valid Parentheses

---
+++ java/hard/32.最长有效括号.java
@@ -0,0 +1,33 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+ * @lc app=leetcode.cn id=32 lang=java
+ *
+ * [32] 最长有效括号
+ */
+
+// @lc code=start
+class Solution {
+    public int longestValidParentheses(String s) {
+        // 三种解法：DP，栈，双指针
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    ans = Math.max(ans, i - stack.peek());
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
